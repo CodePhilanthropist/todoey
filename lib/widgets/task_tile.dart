cac1a774a8aa007181e59ddcbc699a45790bd477
CodePhilanthropist <barrigaete@gmail.com>
2023-03-09 09:28:25 +0800

    push changes in state

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -15,3 +15,25 @@ class TaskTile extends StatelessWidget {
     );
   }
 }
+
+class TaskCheckbox extends StatefulWidget {
+  const TaskCheckbox({super.key});
+
+  @override
+  State<TaskCheckbox> createState() => _TaskCheckboxState();
+}
+
+class _TaskCheckboxState extends State<TaskCheckbox> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: (value) {
+        setState(() {
+          isChecked = value!;
+        });
+      },
+    );
+  }
+}
