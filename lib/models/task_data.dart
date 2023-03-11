7c99629347a37f96029e4360caec35de53269dd9
CodePhilanthropist <barrigaete@gmail.com>
2023-03-11 13:41:19 +0800

    add ChangeNotifierProvider

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,3 +1,15 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'task.dart';
 
-class TaskData extends ChangeNotifier {}
+class TaskData extends ChangeNotifier {
+  List<Task> tasks = [
+    Task(name: "Learn Dart programming language", isDone: true),
+    Task(name: "Understand Flutter architecture and widgets", isDone: true),
+    Task(name: "Build projects", isDone: false),
+    Task(name: "Learn about state management", isDone: false),
+    Task(name: "Master Flutter tools and IDEs", isDone: false),
+    Task(name: "Stay up-to-date with Flutter updates", isDone: false),
+    Task(name: "Participate in the Flutter community", isDone: false),
+    Task(name: "Publish apps", isDone: false),
+  ];
+}
--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -11,17 +11,6 @@ class TaskScreen extends StatefulWidget {
 }
 
 class _TaskScreenState extends State<TaskScreen> {
-  List<Task> tasks = [
-    Task(name: "Learn Dart programming language", isDone: true),
-    Task(name: "Understand Flutter architecture and widgets", isDone: true),
-    Task(name: "Build projects", isDone: false),
-    Task(name: "Learn about state management", isDone: false),
-    Task(name: "Master Flutter tools and IDEs", isDone: false),
-    Task(name: "Stay up-to-date with Flutter updates", isDone: false),
-    Task(name: "Participate in the Flutter community", isDone: false),
-    Task(name: "Publish apps", isDone: false),
-  ];
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
